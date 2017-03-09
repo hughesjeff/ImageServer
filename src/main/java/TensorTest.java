b9fc601932f621dad2dd5a83ed7d902b08d5040e
Jeff Hughes <jeff@jhughes.xyz>
2017-03-08 22:58:24 -0500

    Initial commit

---
+++ src/main/java/TensorTest.java
@@ -0,0 +1,10 @@
+import org.tensorflow.TensorFlow;
+
+/**
+ * Created by jeff on 3/8/17.
+ */
+public class TensorTest {
+    public static void main(String[] args) {
+        System.out.println("I'm using TensorFlow version: " +  TensorFlow.version());
+    }
+}
